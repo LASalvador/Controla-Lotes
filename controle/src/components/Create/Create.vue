3f815c0084a591906017aa68b20efb1879510155
Guilherme <guiiix93@gmail.com>
2019-06-06 10:37:17 -0300

    Atualização na tela de criar lotes

--- controle/src/components/Create/Create.vue
+++ controle/src/components/Create/Create.vue
@@ -7,7 +7,15 @@
       placeholder=" Ex: Prótese dentária">
     </v-text-field>
     <v-text-field v-model="quantidade" type="number" label="Quantidade" append-outer-icon="add" @click:append-outer="increment" prepend-icon="remove" @click:prepend="decrement"></v-text-field>
-
+        <h2> Etapas </h2>
+        <v-checkbox v-model="checkbox1" :label="`Produção`"></v-checkbox>
+        <v-checkbox v-model="checkbox2" :label="`Lavagem`"></v-checkbox>
+        <v-checkbox v-model="checkbox3" :label="`Jateamento`"></v-checkbox>
+        <v-checkbox v-model="checkbox4" :label="`Inspeção`"></v-checkbox>
+        <v-checkbox v-model="checkbox5" :label="`Lavagem 2`"></v-checkbox>
+        <v-checkbox v-model="checkbox6" :label="`Sala limpa`"></v-checkbox>
+        <v-checkbox v-model="checkbox7" :label="`Embalagem`"></v-checkbox>
+      <v-btn icon @click="enviar_dados"></v-btn>
       </v-flex>
     </v-layout>
   </v-content>
--- controle/src/components/Create/CreateController.js
+++ controle/src/components/Create/CreateController.js
@@ -1,6 +1,13 @@
 export default {
     data: () => ({
         quantidade: 0,
+        checkbox1: 0,
+        checkbox2: 0,
+        checkbox3: 0,
+        checkbox4: 0,
+        checkbox5: 0,
+        checkbox6: 0,
+        checkbox7: 0,
     }),
 
     methods: {
@@ -9,6 +16,38 @@ export default {
         },
         decrement() {
             this.foo = parseInt(this.foo, 10) - 1
-        }
+
+        },
+        enviar_dados(){
+            let trajeto;
+            if (this.checkbox1){
+                trajeto += 'Produção;';
+            }
+            if (this.checkbox2){
+                trajeto += 'Lavagem;';
+            }
+            if (this.checkbox3){
+                trajeto += 'Jateamento;';
+            }
+            if (this.checkbox4){
+                trajeto += 'Inspeção;';
+            }
+            if (this.checkbox5){
+                trajeto += 'Lavagem 2;';
+            }
+            if (this.checkbox6){
+                trajeto += 'Sala Limpa;';
+            }
+            if (this.checkbox7){
+                trajeto += 'Embalagem;';
+            }
+        this.$http.post('http://localhost:8081', {
+            produto: this.produto,
+            quantidade: this.quantidade,
+            trajeto: trajeto
+        }).then(function(data) {
+            console.log(data);
+        })},
+
     }
 }
